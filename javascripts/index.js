8898b4341cca240a0c27d0b9774c256ee9f654bc
peterjacobson <peterjacobson.nz@gmail.com>
2015-12-27 04:01:02 +1300

    MLP

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,9 +1,11 @@
 var collapseTriggers = document.getElementsByClassName("collapse-header")
-console.log(collapseTriggers);
+var makeBookingButtong = document.getElementById("make-booking-button")
 
 for (var i = collapseTriggers.length - 1; i >= 0; i--) {
   collapseTriggers[i].addEventListener("click", toggleCollapse) 
 };
+
+makeBookingButtong.addEventListener("click", expandContactSection)
 
 function toggleCollapse(e) {
   var icon = this.children[1]
@@ -15,4 +17,11 @@ function toggleCollapse(e) {
     icon.className = icon.className.replace('minus', 'plus')
     collapsableSection.className = collapsableSection.className.replace('expanded', 'collapsed')
   }
+}
+
+function expandContactSection() {
+  var icon = document.getElementById("contact").children[0].children[1]
+  var collapsableSection = document.getElementById("contact").children[1]
+  icon.className = icon.className.replace('plus', 'minus')
+  collapsableSection.className = collapsableSection.className.replace('collapsed', 'expanded')
 }
