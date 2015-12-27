0a5a7e94f6e75068009c4915d57cd188f28c5c25
peterjacobson <peterjacobson.nz@gmail.com>
2015-12-27 17:00:22 +1300

    animated collapsing sections

--- javascripts/index.js
+++ javascripts/index.js
@@ -12,10 +12,13 @@ function toggleCollapse(e) {
   var collapsableSection = this.parentElement.children[1]
   if (icon.className.match('plus')) {
     icon.className = icon.className.replace('plus', 'minus')
-    Velocity(collapsableSection, { height: '100%' }, { duration: 600, easing: "swing"})
+    collapsableSection.style.height="auto"
+    var fullHeight = collapsableSection.scrollHeight
+    collapsableSection.style.height=0
+    Velocity(collapsableSection, { height: fullHeight }, { duration: 100, easing: "swing"})
   } else {
     icon.className = icon.className.replace('minus', 'plus')
-    Velocity(collapsableSection, { height: 0 }, { duration: 400, easing: "swing"})
+    Velocity(collapsableSection, { height: 0 }, { duration: 100, easing: "swing"})
   }
 }
 
